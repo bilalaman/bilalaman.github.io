3a5529a4e0f9345e13b77a5894681e71ba29dd22
bilalaman <bilal.aman@gmail.com>
2026-03-09 16:19:50 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-var CACHE='bilalaman-v3';
+var CACHE='bilalaman-v4';
 
 self.addEventListener('install',function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){return caches.delete(k);}));
+    })
+  );
   self.skipWaiting();
 });
 
@@ -18,8 +23,6 @@ self.addEventListener('activate',function(e){
 self.addEventListener('fetch',function(e){
   e.respondWith(
     fetch(e.request).then(function(res){
-      var clone=res.clone();
-      caches.open(CACHE).then(function(cache){cache.put(e.request,clone);});
       return res;
     }).catch(function(){
       return caches.match(e.request);
